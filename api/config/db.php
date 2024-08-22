c4367cd328c3d7cd3d3853dabf0f19f5950b0198
max <mzabolotnyi@gmail.com>
2024-08-22 14:23:19 +0200

    add db port

--- api/config/db.php
+++ api/config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'enableSchemaCache' => true,
-    'dsn' => 'mysql:host=' . getenv('DB_HOST') . ';dbname=' . getenv('DB_NAME'),
+    'dsn' => 'mysql:host=' . getenv('DB_HOST') . ';port=' . getenv('DB_PORT') . ';dbname=' . getenv('DB_NAME'),
     'username' => getenv('DB_USERNAME'),
     'password' => getenv('DB_PASSWORD'),
     'charset' => 'utf8',
